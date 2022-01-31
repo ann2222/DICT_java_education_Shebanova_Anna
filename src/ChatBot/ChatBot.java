a3f43938f903ce283cf44abdcf015646dd073848
Shebanova Anna <a.o.shebanova@student.khai.edu>
2022-01-31 07:44:49 +0200

    ChatBot 2 stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,10 +1,22 @@
 package ChatBot;
 
+import java.util.Scanner;
+
 public class ChatBot {
+    final static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        greet();
+        remindName();
+    }
 
-    public static void main(String[] args){
-        System.out.println("Hello! My name is " + "Altron" + ".");
-        System.out.println("I was created in " + "2021" + ".");
-
+    static void greet() {
+        System.out.println("Hello! My name is " + "Aid" + ".");
+        System.out.println("I was created in " + "2018" + ".");
+        System.out.println("Please, remind me your name.");
+    }
+    static void remindName()
+    {
+        String name = scanner.nextLine();
+        System.out.println("What a great name you have, " + name + "!");
     }
 }

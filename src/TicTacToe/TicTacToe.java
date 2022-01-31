9cdcfceb80cfe3e870cdeddd2ae20663582de466
Shebanova Anna <a.o.shebanova@student.khai.edu>
2022-01-31 09:49:27 +0200

    TicTacToe 4 stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -3,71 +3,135 @@ import java.util.Scanner;
 
 
 public class TicTacToe {
-    public static Scanner in = new Scanner(System.in);
-    public static  boolean if_Win_X = false, if_Win_O = false;
+
+    Scanner userInput = new Scanner(System.in);
+    String line1 = "___";
+    String line2 = "___";
+    String line3 = "___";
+    char moveNow = 'X';
+
     public static void main(String[] args) {
-        System.out.print("Enter cells:");
-        String cells = in.nextLine();
-        for (int i = 0; i < cells.length(); i++) {
-            char c = cells.charAt(i);
-            if ((!Character.isDigit(c)) && (!Character.isWhitespace(c))) {
-                if (c == 'X' || c == 'O' || c == '_') {
-                    System.out.println("---------\n" + "| " + cells.charAt(0) + " " + cells.charAt(1) + " " + cells.charAt(2) + " |\n" +
-                            "| " + cells.charAt(3) + " " + cells.charAt(4) + " " + cells.charAt(5) + " |\n" +
-                            "| " + cells.charAt(6) + " " + cells.charAt(7) + " " + cells.charAt(8) + " |\n" + "---------");
-                    statusOfGame(cells);
-                } else {
-                    System.out.println("Enter Only 'X' or 'O' or '_'");
-                    break;
-                }
-            }
+        TicTacToe myGame = new TicTacToe();
+        myGame.setField();
+        myGame.showField();
+        myGame.makeMove();
+        myGame.showField();
+    }
+
+    public int countMatches(String target) {
+        String deleteTarget1 = line1.replace (target, "");
+        String deleteTarget2 = line2.replace (target, "");
+        String deleteTarget3 = line3.replace (target, "");
+        return (line1.length () + line2.length() + line3.length()) - (deleteTarget1.length () + deleteTarget2.length()
+                + deleteTarget3.length());
+    }
+
+    public String checkGameStatus() {
+        boolean xWin = false;
+        boolean oWin = false;
+        if (line1.charAt(0) == 'X' && line1.charAt(1) == 'X' && line1.charAt(2) == 'X' || line2.charAt(0) == 'X' &&
+                line2.charAt(1) == 'X' && line2.charAt(2) == 'X' || line3.charAt(0) == 'X' && line3.charAt(1) == 'X' &&
+                line3.charAt(2) == 'X' || line1.charAt(0) == 'X' && line2.charAt(0) == 'X' && line3.charAt(0) == 'X' ||
+                line1.charAt(1) == 'X' && line2.charAt(1) == 'X' && line3.charAt(1) == 'X' || line1.charAt(2) == 'X' &&
+                line2.charAt(2) == 'X' && line3.charAt(2) == 'X' || line1.charAt(0) == 'X' && line2.charAt(1) == 'X' &&
+                line3.charAt(2) == 'X' || line1.charAt(2) == 'X' && line2.charAt(1) == 'X' && line3.charAt(0) == 'X') {
+            xWin = true;
+        }
+        if (line1.charAt(0) == 'O' && line1.charAt(1) == 'O' && line1.charAt(2) == 'O' || line2.charAt(0) == 'O' &&
+                line2.charAt(1) == 'O' && line2.charAt(2) == 'O' || line3.charAt(0) == 'O' && line3.charAt(1) == 'O' &&
+                line3.charAt(2) == 'O' || line1.charAt(0) == 'O' && line2.charAt(0) == 'O' && line3.charAt(0) == 'O' ||
+                line1.charAt(1) == 'O' && line2.charAt(1) == 'O' && line3.charAt(1) == 'O' || line1.charAt(2) == 'O' &&
+                line2.charAt(2) == 'O' && line3.charAt(2) == 'O' || line1.charAt(0) == 'O' && line2.charAt(1) == 'O' &&
+                line3.charAt(2) == 'O' || line1.charAt(2) == 'O' && line2.charAt(1) == 'O' && line3.charAt(0) == 'O') {
+            oWin = true;
+        }
+        if (countMatches("X") - countMatches("O") > 1 || countMatches("O") -
+                countMatches("X")> 1) {
+            return "Impossible";
+        } else if (xWin && oWin) {
+            return "Impossible";
+        } else if (xWin) {
+            return "X is winner!";
+        } else if (oWin) {
+            return "O is winner!";
+        } else if (line1.contains("_") || line2.contains("_") || line3.contains("_")) {
+            return "Game unfinished";
+        } else {
+            return "Draw";
         }
 
     }
-    public static boolean check_amount(String cells) {
-        int O = 0, X = 0;
-        for (int i = 0; i < 9; i++) {
-            if (cells.charAt(i) == "O".charAt(0)) {
-                O += 1;
-            } else if (cells.charAt(i) == "X".charAt(0)) {
-                X += 1;
+
+    public void makeMove() {
+        System.out.println("Enter coordinates");
+        String coordinates = userInput.nextLine();
+
+        if (moveChecker(coordinates)) {
+            if (coordinates.charAt(0) == '1') {
+                line1 = replaceStringChar(line1, moveNow, Character.getNumericValue(coordinates.charAt(2)) - 1);
+            } else if (coordinates.charAt(0) == '2') {
+                line2 = replaceStringChar(line2, moveNow, Character.getNumericValue(coordinates.charAt(2)) - 1);
+            } else if (coordinates.charAt(0) == '3') {
+                line3 = replaceStringChar(line3, moveNow, Character.getNumericValue(coordinates.charAt(2)) - 1);
             }
         }
-        return Math.abs(O - X) > 1 || Math.abs(X - O) > 1;
     }
-    public static void statusOfGame(String cells) {
-        if (cells.charAt(0) == 'X' && cells.charAt(1) == 'X' && cells.charAt(2) == 'X' || cells.charAt(3) == 'X' &&
-                cells.charAt(4) == 'X' && cells.charAt(5) == 'X' || cells.charAt(6) == 'X' && cells.charAt(7) == 'X' &&
-                cells.charAt(8) == 'X' || cells.charAt(0) == 'X' && cells.charAt(3) == 'X' && cells.charAt(6) == 'X' ||
-                cells.charAt(1) == 'X' && cells.charAt(4) == 'X' && cells.charAt(7) == 'X' || cells.charAt(2) == 'X' &&
-                cells.charAt(5) == 'X' && cells.charAt(8) == 'X' || cells.charAt(0) == 'X' && cells.charAt(4) == 'X' &&
-                cells.charAt(8) == 'X' || cells.charAt(2) == 'X' && cells.charAt(4) == 'X' && cells.charAt(6) == 'X') {
-            if_Win_X = true;
-        }
-        if (cells.charAt(0) == 'O' && cells.charAt(1) == 'O' && cells.charAt(2) == 'O' || cells.charAt(3) == 'O' &&
-                cells.charAt(4) == 'O' && cells.charAt(5) == 'O' || cells.charAt(6) == 'O' && cells.charAt(7) == 'O' &&
-                cells.charAt(8) == 'O' || cells.charAt(0) == 'O' && cells.charAt(3) == 'O' && cells.charAt(6) == 'O' ||
-                cells.charAt(1) == 'O' && cells.charAt(4) == 'O' && cells.charAt(7) == 'O' || cells.charAt(2) == 'O' &&
-                cells.charAt(5) == 'O' && cells.charAt(8) == 'O' || cells.charAt(0) == 'O' && cells.charAt(4) == 'O' &&
-                cells.charAt(8) == 'O' || cells.charAt(2) == 'O' && cells.charAt(4) == 'O' && cells.charAt(6) == 'O') {
-            if_Win_O = true;
-        }
-        if (check_amount(cells) || (if_Win_O && if_Win_X)) {
-            System.out.println("It's Impossible");
-            System.exit( 0);
-        }
-        if (if_Win_O) {
-            System.out.println("O wins");
-            System.exit( 0);
-        }
-        if (if_Win_X) {
-            System.out.println("X wins");
-            System.exit( 0);
-        }
-        if (cells.contains("_")) {
-            System.out.println("Game not finished");
+
+    public String replaceStringChar(String string, char character, int index) {
+        return string.substring(0, index) + character + string.substring(index+1);
+    }
+
+    public boolean moveChecker (String coordinates) {
+        if (coordinates.length() != 3) {
+            System.out.println("You should enter two numbers separated by space!");
+            return false;
+        } else if (!(Character.isDigit(coordinates.charAt(0)) && Character.isDigit(coordinates.charAt(2)))) {
+            System.out.println("You should enter numbers");
+            return false;
+        } else if (!(Character.getNumericValue(coordinates.charAt(0)) <= 3
+                && Character.getNumericValue(coordinates.charAt(0)) <= 3)) {
+            System.out.println("Numbers should be from 1 to 3");
+            return false;
+        } else if (Character.getNumericValue(coordinates.charAt(0)) == 1) {
+            if (line1.charAt(Character.getNumericValue(coordinates.charAt(2)) - 1) != '_') {
+                System.out.println("This cell is occupied, choose another one");
+                return false;
+            } else {
+                return true;
+            }
+        } else if (Character.getNumericValue(coordinates.charAt(0)) == 2) {
+            if (line2.charAt(Character.getNumericValue(coordinates.charAt(2)) - 1) != '_') {
+                System.out.println("This cell is occupied, choose another one");
+                return false;
+            } else {
+                return true;
+            }
+        } else if (Character.getNumericValue(coordinates.charAt(0)) == 3) {
+            if (line3.charAt(Character.getNumericValue(coordinates.charAt(2)) - 1) != '_') {
+                System.out.println("This cell is occupied, choose another one");
+                return false;
+            } else {
+                return true;
+            }
         } else {
-            System.out.println("Draw");
+            return false;
         }
+    }
+
+    public void setField() {
+        System.out.println("Enter cells:");
+        String newField = userInput.nextLine();
+        line1 = newField.substring(0,3);
+        line2 = newField.substring(3, 6);
+        line3 = newField.substring(6, 9);
+    }
+
+    public void showField() {
+        System.out.printf("---------\n" +
+                "| %s |\n" +
+                "| %s |\n" +
+                "| %s |\n" + "---------\n", line1, line2, line3);
     }
 }
+
+

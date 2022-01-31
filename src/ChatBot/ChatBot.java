26c46c844917ee4d2c5c468ee10ca4b629bdaad5
Shebanova Anna <a.o.shebanova@student.khai.edu>
2022-01-31 07:43:17 +0200

    ChatBot 1 stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,10 @@
+package ChatBot;
+
+public class ChatBot {
+
+    public static void main(String[] args){
+        System.out.println("Hello! My name is " + "Altron" + ".");
+        System.out.println("I was created in " + "2021" + ".");
+
+    }
+}

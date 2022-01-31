0f680f56b34774b1c4e7990c8f53324f0a32afcd
Shebanova Anna <a.o.shebanova@student.khai.edu>
2022-01-31 09:49:02 +0200

    TicTacToe 2 stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,7 +1,11 @@
 package TicTacToe;
+import java.util.Scanner;
 
 public class TicTacToe {
     public static void main(String[] args) {
-        System.out.println("X O X \n"+"O X O\n"+"X X O");
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter cells:");
+        String cells = userInput.nextLine();
+        System.out.printf("---------\n" +"| %s |\n" +"| %s |\n" +"| %s |\n" +"---------", cells.substring(0, 3), cells.substring(3, 6), cells.substring(6, 9));
     }
 }

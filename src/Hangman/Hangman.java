0b29a0fc6b53d8669fe995d5b364b8a5171fd1ba
Shebanova Anna <a.o.shebanova@student.khai.edu>
2022-01-31 08:54:12 +0200

    Hangman 6 stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,14 +1,12 @@
 package Hangman;
-
-import java.util.Random;
 import java.util.Scanner;
-
+import java.util.Random;
 public class Hangman {
-
     public static void main(String[] args) {
         String[] wordsArr = {"java", "kotlin", "python", "javascript"};
         String word = generateWord(wordsArr);
         StringBuilder hiddenWord = hiddenWordGen(word);
+        StringBuilder chosenLetters = new StringBuilder("");
         System.out.println("HANGMAN");
         int lives = 8;
         while (true) {
@@ -20,16 +18,14 @@ public class Hangman {
                     System.out.printf("Guess the word %s:", hiddenWord);
                     Scanner userInput = new Scanner(System.in);
                     String answer = userInput.nextLine();
-                    if (!word.contains(answer)) {
-                        System.out.println("The letter doesn't appear in word");
-                        --lives;
-                    }
-                    else if (hiddenWord.indexOf(answer) != -1){
-                        System.out.println("No improvements");
-                        --lives;
-                    }
-                    else {
-                        hiddenWord = updateHiddenWord(hiddenWord, answer, word);
+                    if (checker(answer, chosenLetters)) {
+                        chosenLetters.append(answer);
+                        if (!word.contains(answer)) {
+                            System.out.println("The letter doesn't appear in word");
+                            --lives;
+                        } else {
+                            hiddenWord = updateHiddenWord(hiddenWord, answer, word);
+                        }
                     }
                 } else {
                     System.out.println(hiddenWord);
@@ -37,22 +33,18 @@ public class Hangman {
                     break;
                 }
             }
-
         }
     }
-
     public static String generateWord(String[] wordsArr) {
         Random randomIntGen = new Random();
         int randomInt = randomIntGen.nextInt(wordsArr.length);
         return wordsArr[randomInt];
     }
-
     public static StringBuilder hiddenWordGen(String word) {
         StringBuilder hiddenWord = new StringBuilder("");
         hiddenWord.append("-".repeat(word.length()));
         return hiddenWord;
     }
-
     public static StringBuilder updateHiddenWord(StringBuilder hiddenWord, String answer, String  word) {
         int index = 0;
         while (index >= 0) {
@@ -63,5 +55,26 @@ public class Hangman {
             }
         }
         return hiddenWord;
+    }
+
+    public static Boolean checker(String answer, StringBuilder chosen_letters) {
+        String letters = "qwertyuiopasdfghjklzxcvbnm";
+        if (answer.length() != 1) {
+            System.out.println("You should enter one letter");
+            return false;
+        }
+        else if (!letters.contains(answer)){
+            System.out.println("You should enter only english lowercase letters!");
+            return false;
+        }
+        else if (chosen_letters.indexOf(answer) != -1){
+            System.out.println("You already choose that letter");
+            return false;
+        }
+        else {
+            return true;
+        }
+
+
     }
 }
